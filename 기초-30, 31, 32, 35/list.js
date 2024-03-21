95a8afd530e82fb95568af1bd8abaaf22987e053
Bucky <bucky5683@gmail.com>
2024-03-21 16:47:37 +0900

    JavaScript 기초 35

--- 기초-30, 31, 32, 35/list.js
+++ 기초-30, 31, 32, 35/list.js
@@ -1,17 +1,19 @@
-
 var products = [
-    { id : 0, price : 70000, title : 'Blossom Dress' },
-    { id : 1, price : 50000, title : 'Springfield Shirt' },
-    { id : 2, price : 60000, title : 'Black Monastery' }
+    {id: 0, price: 70000, title: 'Blossom Dress'},
+    {id: 1, price: 50000, title: 'Springfield Shirt'},
+    {id: 2, price: 60000, title: 'Black Monastery'}
 ];
+let buyCart = [];
 let moreButtonCount = 0;
 let sortCardABC = true;
-
+var 꺼낸거 = localStorage.getItem('cart');
+꺼낸거 = JSON.parse(꺼낸거)
+buyCart = 꺼낸거
 $('.row').html('');
 
 addCardHtml(products);
 
-$('#more').click(function(){
+$('#more').click(function () {
     moreButtonCount++;
     $('.row').html('');
     if (moreButtonCount == 1) {
@@ -20,7 +22,7 @@ $('#more').click(function(){
                 data.forEach(function (product) {
                     products.push(product);
                 });
-                addCardHtml()
+                addCardHtml(products)
             });
     } else if (moreButtonCount == 2) {
         $.get('https://codingapple1.github.io/js/more2.json')
@@ -33,7 +35,7 @@ $('#more').click(function(){
     }
 });
 
-$('#sort-button').click(function(){
+$('#sort-button').click(function () {
     $('.row').html('');
     if (sortCardABC) {
         products.sort(function compare(a, b) {
@@ -55,37 +57,59 @@ $('#sort-button').click(function(){
     addCardHtml(products)
 });
 
-$('#filter-button').click(function() {
+$('#filter-button').click(function () {
     $('.row').html('');
-    var newProducts = products.filter(function (a){
+    var newProducts = products.filter(function (a) {
         return a.price < 60000
     })
-    addCardHtml(newProducts)
-})
+    addCardHtml(newProducts);
+});
+
+$(document).on('click', '.card-buy', function (e) {
+    var title = $(e.target).siblings('h5').text();
+    if (localStorage.getItem('cart') != null) {
+        var 꺼낸거 = JSON.parse(localStorage.cart);
+        꺼낸거.push(title);
+        localStorage.setItem('cart', JSON.stringify(꺼낸거));
+    } else {
+        localStorage.setItem('cart', JSON.stringify([title]));
+    }
+});
 
 function addCardHtml(array) {
     //카드 전체
-    array.forEach(function(product, _) {
+    array.forEach(function (product, _) {
         let cardhtml = document.createElement('div');
         cardhtml.classList.add('col-sm-4');
 
         //이미지 추가
         let cardImage = document.createElement('img');
         cardImage.classList.add("w-100");
+        cardImage.id = product.id;
         cardImage.src = "https://via.placeholder.com/600"
         cardhtml.appendChild(cardImage);
 
         //카드 제목 추가
         let cardTitle = document.createElement('h5');
         cardTitle.classList.add('card-title');
+        cardTitle.id = product.id;
         cardTitle.innerHTML = product.title;
         cardhtml.appendChild(cardTitle);
 
         //가격 추가
         let cardPrice = document.createElement('p');
         cardPrice.classList.add('card-price');
+        cardTitle.id = product.id;
         cardPrice.innerHTML = "가격 : " + product.price;
         cardhtml.appendChild(cardPrice);
+
+        let cardAddCart = document.createElement('button');
+        cardAddCart.classList.add('btn');
+        cardAddCart.classList.add('btn-success');
+        cardAddCart.classList.add('card-buy');
+        cardAddCart.id = product.id;
+        cardAddCart.innerText = "구매하기";
+        cardhtml.appendChild(cardAddCart);
 
         //카드 html에 추가
         $('.row').append(cardhtml);
